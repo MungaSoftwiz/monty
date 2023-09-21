47d0ab70a4be657a2d22bbebd09feded7587cf3c
MungaSoftwiz <mwachilumobm@gmail.com>
2023-09-21 12:08:47 +0300

    Update _pint.c file

--- _pint.c
+++ _pint.c
@@ -1,11 +1,16 @@
 #include "monty.h"
 
+/**
+ * _pint - function prints the top element in the stack
+ * @stack: a double pointer to the top of the stack
+ * @line_number: the line number of the opcode
+ */
 void _pint(s_node *stack, unsigned int line_number)
 {
-	if (!(my_node->current))
+	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	printf("%d\n", my_node->current->n);
+	printf("%d\n", (*stack)->n);
 }
