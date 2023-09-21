8c3316828e3845a19c2ef8a223f030aaec1c8a7c
root <effahfrancis210@gmail.com>
2023-09-21 05:21:11 +0000

    update

--- monty.c
+++ monty.c
@@ -15,28 +15,28 @@ int main(int ac, char **av)
 	char *op = NULL;
 	char line[LINE_LENGTH]; 
 
-    if (ac != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        return (EXIT_FAILURE);
-    }
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		return (EXIT_FAILURE);
+	}
 
-    file = fopen(av[1], "r");
-    
-    if (!file)
-    {
-        perror(av[1]);
-        return (EXIT_FAILURE);
-    }
+	file = fopen(av[1], "r");
 
-    my_struct my_node = {0};
-    my_node.fd = file;
+	if (!file)
+	{
+		perror(av[1]);
+		return (EXIT_FAILURE);
+	}
 
-    s_node stack = NULL;
+	my_struct my_node = {0};
+	my_node.fd = file;
 
-    while (fgets(line, sizeof(line), file))
-    {
-        op = strtok(line, " \t");
+	s_node stack = NULL;
+
+	while (fgets(line, sizeof(line), file))
+	{
+		op = strtok(line, " \t");
         if (_comment(op))
         {
             handle_opcode(&stack, strlen(op), op, &(my_node.line_num));
