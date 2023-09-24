26abfb0ee98abbb1dce88fff0e111f9b667854e7
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-24 12:10:47 +0000

    Create stack_and_queue.c

---
+++ stack_and_queue.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * stack - sets the format of the data to a stack (LIFO)
+ * @stack: Pointer to stack
+ * @line_num: Line number to the code
+ */
+
+void stack(s_node **stack, unsigned int line_num)
+{
+	(void)stack;
+	(void)line_num;
+}
+
+
+/**
+ * queue - sets the format of the data to a queue (FIFO)
+ * @stack: Pointer to stack
+ * @line_num: Line number to the code
+ */
+
+void queue(s_node **stack, unsigned int line_num)
+{
+	(void)stack;
+	(void)line_num;
+}
