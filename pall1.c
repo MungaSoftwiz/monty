718bbee347d2ac089ff96883858cb32e60b4179d
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-24 07:03:03 +0000

    Update pall1.c

--- pall1.c
+++ pall1.c
@@ -1,5 +1,10 @@
 #include "monty.h"
 
+/**
+ * pall - prints all the values on the stack
+ * @stack: Pointer to a stack
+ * @line_number: Line number of the command 
+ */
 void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current = *stack;
