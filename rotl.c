5bdf39c4cfe70ae9def08f7dae3a3b7928489408
MungaSoftwiz <mwachilumobm@gmail.com>
2023-09-25 03:34:44 +0300

    Update rotate function

--- rotl.c
+++ rotl.c
@@ -1,27 +1,28 @@
 #include "monty.h"
 
 /**
- * div - divide the elements of the stack
+ * rotl - rotate the elements of the stack
  * @stack: Pointer to a stack
- * @line_number: Line number of the command 
+ * @line_number: Line number of the command
  */
 
 void rotl(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp1 = NULL;
-	stack_t *tmp2 = NULL;
+	(void)line_number;
 
-	if (*stack == NULL || (*stack)->next == NULL)
-		return;
-
-	tmp1 = (*stack)->next;
-	tmp2 = *stack;
-
-	for (tmp2->next != NULL; tmp2 = tmp2->next)
+	if (*stack != NULL && (*stack)->next != NULL)
 	{
-		tmp1->prev = NULL;
+		stack_t *tmp1 = *stack;
+		stack_t *tmp2 = *stack;
+
+		while (tmp2->next != NULL)
+		{
+			tmp2 = tmp2->next;
+		}
 		tmp2->next = *stack;
-		(*stack)->next = NULL;
 		(*stack)->prev = tmp2;
-		*stack = tmp1;
+		*stack = tmp1->next;
+		(*stack)->prev = NULL;
+		tmp1->next = NULL;
 	}
+}
